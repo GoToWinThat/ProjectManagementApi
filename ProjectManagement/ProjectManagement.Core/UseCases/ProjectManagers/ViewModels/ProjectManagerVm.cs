b9ede4727e03f883673c22e70558b761a1eb3116
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 21:30:56 +0200

    Created project manager vm

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/ViewModels/ProjectManagerVm.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ProjectManagement.Core.UseCases.ProjectManagers.DTO;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.ViewModels
+{
+    public class ProjectManagerVm
+    {
+        public IList<ProjectManagerDto> ProjectManagersList { get; set; }
+        public int Count { get; set; }
+    }
+}
