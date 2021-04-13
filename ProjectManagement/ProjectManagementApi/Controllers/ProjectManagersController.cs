6d4de78a938e32accda9a1892ec2cee48f80b323
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 22:32:36 +0200

    implemented Creating ProjectManager in PM controller

--- ProjectManagement/ProjectManagementApi/Controllers/ProjectManagersController.cs
+++ ProjectManagement/ProjectManagementApi/Controllers/ProjectManagersController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.ProjectManagers.Commands.CreateProjectManager;
+using ProjectManagement.Core.UseCases.ProjectManagers.DTO;
 using ProjectManagement.Core.UseCases.ProjectManagers.Queries.GetProjectManagers;
 using ProjectManagement.Core.UseCases.ProjectManagers.ViewModels;
 
@@ -11,6 +13,14 @@ namespace ProjectManagementApi.Controllers
         public async Task<ActionResult<ProjectManagerVm>> GetAllProjectManagers()
         {
             return await Mediator.Send(new GetProjectManagersQuery());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<DetailedProjectManagerDto>> AddProjectManager(
+            [FromBody] CreateProjectManagerCommand createProjectManagerCommand)
+        {
+            var result = await Mediator.Send(createProjectManagerCommand);
+            return Ok(result.DetailedProjectManagerDto);
         }
     }
 }
