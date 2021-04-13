699493a6625ba7d90902c766bdcd891e2e6abe9f
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 21:30:38 +0200

    Created project manager dto

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/DTO/ProjectManagerDto.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.DTO
+{
+    public class ProjectManagerDto : IMapFrom<ProjectManager>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+        public void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<ProjectManager, ProjectManagerDto>().ReverseMap();
+        }
+    }
+}
