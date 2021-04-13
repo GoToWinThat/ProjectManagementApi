2aab58beaef490e69a59e318517d53ad9d2ac4a3
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 22:31:50 +0200

    Created CreateProjectManager Command Response

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Commands/CreateProjectManager/CreateProjectManagerCommandResponse.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+using ProjectManagement.Core.UseCases.ProjectManagers.DTO;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Commands.CreateProjectManager
+{
+    public class CreateProjectManagerCommandResponse : BaseResponse
+    {
+        public DetailedProjectManagerDto DetailedProjectManagerDto { get; set; }
+
+        public CreateProjectManagerCommandResponse() : base() { }
+        public CreateProjectManagerCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+        public CreateProjectManagerCommandResponse(string message) : base(message) { }
+        public CreateProjectManagerCommandResponse(string message, bool success) : base(message, success) { }
+        public CreateProjectManagerCommandResponse(DetailedProjectManagerDto detailedProjectManagerDto) => DetailedProjectManagerDto = detailedProjectManagerDto;
+    }
+}
