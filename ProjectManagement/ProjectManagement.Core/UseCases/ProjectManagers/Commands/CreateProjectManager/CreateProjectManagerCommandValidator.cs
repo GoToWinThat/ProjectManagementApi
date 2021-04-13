8b86a14086bfe13792b5de312c12aebe6b9be6b0
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 22:31:59 +0200

    Created CreateProjectManager Command Response

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Commands/CreateProjectManager/CreateProjectManagerCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Commands.CreateProjectManager
+{
+    public class CreateProjectManagerCommandValidator : AbstractValidator<CreateProjectManagerCommand>
+    {
+        public CreateProjectManagerCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+
+            RuleFor(c => c.Surname).NotEmpty();
+
+            RuleFor(c => c.Email).EmailAddress();
+
+            RuleFor(c => c.PhoneNumber).NotEmpty();
+            
+            RuleFor(c => c.BossId)
+                .GreaterThan(0)
+                .WithMessage("Id must be more than zero");
+        }
+    }
+}
