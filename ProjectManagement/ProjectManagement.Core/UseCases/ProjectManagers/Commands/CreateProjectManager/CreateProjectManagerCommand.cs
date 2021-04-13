b035a4a14e98f7502388e8fc23844b039858a3e7
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 22:31:37 +0200

    Created CreateProjectManager Command

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Commands/CreateProjectManager/CreateProjectManagerCommand.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Commands.CreateProjectManager
+{
+    public class CreateProjectManagerCommand : IRequest<CreateProjectManagerCommandResponse>, IMapFrom<ProjectManager>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int BossId { get; set; }
+
+        public static void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<CreateProjectManagerCommand, ProjectManager>();
+        }
+    }
+}
