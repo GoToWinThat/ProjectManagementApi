71b86ff3d9eadbb704fb9bf48d69051be75efe17
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 21:46:44 +0200

    Implemented getting Project Managers in Project Managers controller

---
+++ ProjectManagement/ProjectManagementApi/Controllers/ProjectManagersController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.ProjectManagers.Queries.GetProjectManagers;
+using ProjectManagement.Core.UseCases.ProjectManagers.ViewModels;
+
+namespace ProjectManagementApi.Controllers
+{
+    public class ProjectManagersController : ApiControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<ProjectManagerVm>> GetAllProjectManagers()
+        {
+            return await Mediator.Send(new GetProjectManagersQuery());
+        }
+    }
+}
