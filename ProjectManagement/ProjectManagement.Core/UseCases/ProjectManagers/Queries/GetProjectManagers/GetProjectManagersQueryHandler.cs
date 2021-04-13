cd626155b685203ca6d7962df8280da5d0bd0900
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 21:46:12 +0200

    Created Get Project Managers Query Handler

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Queries/GetProjectManagers/GetProjectManagersQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using ProjectManagement.Core.Base.Interfaces;
+using ProjectManagement.Core.UseCases.ProjectManagers.DTO;
+using ProjectManagement.Core.UseCases.ProjectManagers.ViewModels;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Queries.GetProjectManagers
+{
+    public class GetProjectManagersQueryHandler : IRequestHandler<GetProjectManagersQuery, ProjectManagerVm>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetProjectManagersQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        public async Task<ProjectManagerVm> Handle(GetProjectManagersQuery request, CancellationToken cancellationToken)
+        {
+            var projectManagers = 
+                await _context.ProjectManagers
+                    .ProjectTo<ProjectManagerDto>(_mapper.ConfigurationProvider)
+                    .ToListAsync(cancellationToken);
+            return new()
+            {
+                ProjectManagersList = projectManagers,
+                Count = projectManagers.Count
+            };
+        }
+    }
+}
