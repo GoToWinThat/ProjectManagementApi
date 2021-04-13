d1545ded53bfdd47d6c43c149526d06ee0f04a86
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-13 21:30:47 +0200

    Created detailed project manager dto

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/DTO/DetailedProjectManagerDto.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Domain.Entities;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.DTO
+{
+    public class DetailedProjectManagerDto : IMapFrom<ProjectManager>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public int BossId { get; set; }
+        
+        public void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<ProjectManager, DetailedProjectManagerDto>();
+        }
+    }
+}
