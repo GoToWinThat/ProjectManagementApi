cd804dbd69479a0b804b11dd4cd52890091dcddd
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-15 21:54:11 +0200

    Created delete project manager command handler

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Commands/DeleteProjectManager/DeleteProjectManagerCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Exceptions;
+using ProjectManagement.Core.Base.Interfaces;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Commands.DeleteProjectManager
+{
+    public class DeleteProjectManagerCommandHandler : IRequestHandler<DeleteProjectManagerCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteProjectManagerCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
+        public async Task<Unit> Handle(DeleteProjectManagerCommand request, CancellationToken cancellationToken)
+        {
+            var projectManager = await _context.ProjectManagers.FindAsync(request.ProjectManagerId, cancellationToken);
+            if (projectManager == null)
+            {
+                throw new NotFoundException(nameof(ProjectManager), request.ProjectManagerId);
+            }
+            
+            _context.ProjectManagers.Remove(projectManager);
+            await _context.SaveChangesAsync(cancellationToken);
+            
+            return Unit.Value;
+        }
+    }
+}
