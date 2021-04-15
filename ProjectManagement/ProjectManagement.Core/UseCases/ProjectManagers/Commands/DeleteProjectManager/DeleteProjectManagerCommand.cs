d44b42d737ad85a716132172b931594642d890dc
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-15 21:54:06 +0200

    Created delete project manager command

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Commands/DeleteProjectManager/DeleteProjectManagerCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Commands.DeleteProjectManager
+{
+    public class DeleteProjectManagerCommand : IRequest
+    {
+        public int ProjectManagerId { get; set; }
+    }
+}
