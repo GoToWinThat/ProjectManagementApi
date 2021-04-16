0027eac01573ed64c1590fbe335b307758cb523d
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-16 18:21:47 +0200

    implemented get project manager by id query in pm controller

--- ProjectManagement/ProjectManagementApi/Controllers/ProjectManagersController.cs
+++ ProjectManagement/ProjectManagementApi/Controllers/ProjectManagersController.cs
@@ -1,9 +1,12 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using ProjectManagement.Core.UseCases.ProjectManagers.Commands.CreateProjectManager;
+using ProjectManagement.Core.UseCases.ProjectManagers.Commands.DeleteProjectManager;
 using ProjectManagement.Core.UseCases.ProjectManagers.DTO;
+using ProjectManagement.Core.UseCases.ProjectManagers.Queries.GetProjectManagerById;
 using ProjectManagement.Core.UseCases.ProjectManagers.Queries.GetProjectManagers;
 using ProjectManagement.Core.UseCases.ProjectManagers.ViewModels;
+using ProjectManagement.Core.UseCases.Projects.Commands.DeleteProject;
 
 namespace ProjectManagementApi.Controllers
 {
@@ -14,6 +17,16 @@ namespace ProjectManagementApi.Controllers
         {
             return await Mediator.Send(new GetProjectManagersQuery());
         }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DetailedProjectManagerDto>> GetAllProjectManagerById(int id)
+        {
+            var getProjectManagerByIdQuery = new GetProjectManagerByIdQuery()
+            {
+                ProjectManagerId = id
+            };
+            return await Mediator.Send(getProjectManagerByIdQuery);
+        }
 
         [HttpPost]
         public async Task<ActionResult<DetailedProjectManagerDto>> AddProjectManager(
@@ -21,6 +34,18 @@ namespace ProjectManagementApi.Controllers
         {
             var result = await Mediator.Send(createProjectManagerCommand);
             return Ok(result.DetailedProjectManagerDto);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteProjectManager(int id)
+        {
+            var deleteProjectManagerCommand = new DeleteProjectManagerCommand()
+            {
+                ProjectManagerId = id
+            };
+            await Mediator.Send(deleteProjectManagerCommand);
+            
+            return NoContent();
         }
     }
 }
