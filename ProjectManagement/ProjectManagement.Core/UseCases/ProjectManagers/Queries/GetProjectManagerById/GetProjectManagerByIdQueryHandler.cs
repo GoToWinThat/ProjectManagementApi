daa7192892654698cf80a7f12d9fe5ed0f0819e9
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-16 18:21:28 +0200

    Created get project manager by id query handler

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Queries/GetProjectManagerById/GetProjectManagerByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Exceptions;
+using ProjectManagement.Core.Base.Interfaces;
+using ProjectManagement.Core.UseCases.ProjectManagers.DTO;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Queries.GetProjectManagerById
+{
+    public class GetProjectManagerQueryHandler : IRequestHandler<GetProjectManagerByIdQuery, DetailedProjectManagerDto>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public GetProjectManagerQueryHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        public async Task<DetailedProjectManagerDto> Handle(GetProjectManagerByIdQuery request, CancellationToken cancellationToken)
+        {
+            var projectManager = await _context.ProjectManagers.FindAsync(request.ProjectManagerId, cancellationToken);
+            if (projectManager == null)
+            {
+                throw new NotFoundException(nameof(ProjectManager), request.ProjectManagerId);
+            }
+
+            var projectManagerToReturn = _mapper.Map<DetailedProjectManagerDto>(projectManager);
+            return projectManagerToReturn;
+        }
+    }
+}
