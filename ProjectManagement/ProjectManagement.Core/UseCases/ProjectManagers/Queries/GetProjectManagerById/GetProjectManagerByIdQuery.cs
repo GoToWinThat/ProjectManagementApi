149500907559d0c33279bf3cad89ff909b03dd6d
Wojtek Kajstura <wojtek.kajstura@gmail.com>
2021-04-16 18:21:20 +0200

    Created get project manager by id query

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/ProjectManagers/Queries/GetProjectManagerById/GetProjectManagerByIdQuery.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.UseCases.ProjectManagers.DTO;
+
+namespace ProjectManagement.Core.UseCases.ProjectManagers.Queries.GetProjectManagerById
+{
+    public class GetProjectManagerByIdQuery : IRequest<DetailedProjectManagerDto>
+    {
+        public int ProjectManagerId { get; set; }
+    }
+}
