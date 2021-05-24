688a90511accb1439ed03eea8ec63e0713901791
Konrad <konradlubera@hotmail.com>
2021-05-24 19:21:23 +0200

    Created create employee command handler

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Employees/Command/CreateEmployee/CreateEmployeeCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoMapper;
+using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Interfaces;
+using ProjectManagement.Core.UseCases.Employees.Dto;
+
+namespace ProjectManagement.Core.UseCases.Employees.Command.CreateEmployee
+{
+    public class CreateEmployeeCommandHandler : IRequestHandler<CreateEmployeeCommand, CreateEmployeeCommandResponse>
+    {
+        private readonly IApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public CreateEmployeeCommandHandler(IApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+        
+        public async Task<CreateEmployeeCommandResponse> Handle(CreateEmployeeCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new CreateEmployeeCommandValidator();
+            var validatorResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validatorResult.IsValid)
+            {
+                return new CreateEmployeeCommandResponse(validatorResult);
+            }
+
+            var employee = _mapper.Map<Employee>(request);
+
+            await _context.Employees.AddAsync(employee, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            var employeeToReturn = _mapper.Map<EmployeeDto>(employee);
+
+            return new(employeeToReturn);
+
+        }
+    }
+}
