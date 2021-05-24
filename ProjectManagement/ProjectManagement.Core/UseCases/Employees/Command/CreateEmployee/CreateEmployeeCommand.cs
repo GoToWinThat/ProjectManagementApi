373112b35055f6fa2b5965fe352b4bc9d7206838
Konrad <konradlubera@hotmail.com>
2021-05-24 19:20:57 +0200

    Created create employee command

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Employees/Command/CreateEmployee/CreateEmployeeCommand.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using MediatR;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.Employees.Command.CreateEmployee
+{
+    public class CreateEmployeeCommand : IRequest<CreateEmployeeCommandResponse>, IMapFrom<Employee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public int AssignId { get; set; }
+
+        public static void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<CreateEmployeeCommand, Employee>();
+        }
+    }
+}
