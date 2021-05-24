2a3477c1fb822be4e940bdff4563f2977000e2f1
Konrad <konradlubera@hotmail.com>
2021-05-24 17:56:04 +0200

    Created employee dto

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Employees/Dto/EmployeeDto.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.Employees.Dto
+{
+    public class EmployeeDto : IMapFrom<Employee>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public int AssignId { get; set; }
+
+        public static void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<Employee, EmployeeDto>();
+        }
+
+    }
+}
