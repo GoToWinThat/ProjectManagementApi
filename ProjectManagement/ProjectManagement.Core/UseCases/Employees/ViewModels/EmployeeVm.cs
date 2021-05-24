45d194ea796971261237349ddc324e7dbbb26e27
Konrad <konradlubera@hotmail.com>
2021-05-24 17:59:13 +0200

    Created employee vm

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Employees/ViewModels/EmployeeVm.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using ProjectManagement.Core.UseCases.Employees.Dto;
+
+namespace ProjectManagement.Core.UseCases.Employees.ViewModels
+{
+    public class EmployeeVm
+    {
+        public IList<EmployeeDto> Employees { get; set; }
+        public int Count { get; set; }
+    }
+}
