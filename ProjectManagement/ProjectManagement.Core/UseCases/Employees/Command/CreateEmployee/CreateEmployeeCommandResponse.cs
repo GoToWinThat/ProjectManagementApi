1516291e3424f5c4af0a83b88c70d961c1d9a263
Konrad <konradlubera@hotmail.com>
2021-05-24 19:20:25 +0200

    Created create employee command response.

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Employees/Command/CreateEmployee/CreateEmployeeCommandResponse.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+using ProjectManagement.Core.UseCases.Employees.Dto;
+
+namespace ProjectManagement.Core.UseCases.Employees.Command.CreateEmployee
+{
+    public class CreateEmployeeCommandResponse : BaseResponse
+    {
+        public EmployeeDto EmployeeDto { get; set; }
+        
+        public CreateEmployeeCommandResponse() : base() { }
+        public CreateEmployeeCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+        public CreateEmployeeCommandResponse(string message) : base(message) { }
+        public CreateEmployeeCommandResponse(string message, bool success) : base(message, success) { }
+        public CreateEmployeeCommandResponse(EmployeeDto employeeDto) => EmployeeDto = employeeDto;
+
+    }
+}
