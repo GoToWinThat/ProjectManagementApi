92cd9f87509a5ed0970ed13b4199d8458bbbbb53
Konrad <konradlubera@hotmail.com>
2021-05-24 19:22:02 +0200

    Created employee controller and implemented create employee command

---
+++ ProjectManagement/ProjectManagementApi/Controllers/EmployeesController.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ProjectManagement.Core.UseCases.Employees.Command.CreateEmployee;
+using ProjectManagement.Core.UseCases.Employees.Dto;
+
+namespace ProjectManagementApi.Controllers
+{
+    public class EmployeesController : ApiControllerBase
+    {
+        [HttpPost]
+        public async Task<ActionResult<EmployeeDto>> AddEmployee([FromBody] CreateEmployeeCommand createEmployeeCommand)
+        {
+            var result = await Mediator.Send(createEmployeeCommand);
+            return Ok(result.EmployeeDto);
+        }
+    }
+}
