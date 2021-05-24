2f449de0a589e57cf76289f748fa732fd3062de8
Konrad <konradlubera@hotmail.com>
2021-05-24 19:20:40 +0200

    Created create employee command validator

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Employees/Command/CreateEmployee/CreateEmployeeCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+
+namespace ProjectManagement.Core.UseCases.Employees.Command.CreateEmployee
+{
+    public class CreateEmployeeCommandValidator : AbstractValidator<CreateEmployeeCommand>
+    {
+        public CreateEmployeeCommandValidator()
+        {
+            RuleFor(c => c.Name).NotEmpty();
+
+            RuleFor(c => c.Surname).NotEmpty();
+
+            RuleFor(c => c.Email).EmailAddress();
+
+            RuleFor(c => c.PhoneNumber).NotEmpty();
+        }
+    }
+}
