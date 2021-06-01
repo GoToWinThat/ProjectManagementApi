ae2f9f4ed7f99ba977bbdfe4cee3f30b716b8411
Konrad <konradlubera@hotmail.com>
2021-06-01 23:46:07 +0200

    Created AssignDto

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Assigns/Dto/AssignDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Domain.Entities;
+using ProjectManagement.Core.Base.Mappings;
+
+namespace ProjectManagement.Core.UseCases.Assigns.Dto
+{
+    public class AssignDto : IMapFrom<Assign>
+    {
+        public int Id { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public int TaskId { get; set; }
+        
+        public int ProjectManagerId { get; set; }
+        public ProjectManager ProjectManager { get; set; }
+        
+        public Role Role { get; set; }
+        public Employee Employee { get; set; }
+
+        public static void Mapping(MappingProfile profile)
+        {
+            profile.CreateMap<Assign, AssignDto>().ReverseMap();
+        }
+    }
+}
