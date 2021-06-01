2dfa3e76aa25bc97a81a6cc93f4d50a9e6dbafde
Konrad <konradlubera@hotmail.com>
2021-06-01 23:46:30 +0200

    Created AssignVm

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Assigns/ViewModels/AssignVm.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using ProjectManagement.Core.UseCases.Assigns.Dto;
+
+namespace ProjectManagement.Core.UseCases.Assigns.ViewModels
+{
+    public class AssignVm
+    {
+        public IList<AssignDto> AssignDto { get; set; }
+        public int Count { get; set; }
+    }
+}
