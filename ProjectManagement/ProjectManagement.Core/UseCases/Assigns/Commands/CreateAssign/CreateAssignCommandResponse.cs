4549446fa28d6019f4a3e26dc07d149048c731d9
Konrad <konradlubera@hotmail.com>
2021-06-02 00:05:17 +0200

    Created create assign command validator

---
+++ ProjectManagement/ProjectManagement.Core/UseCases/Assigns/Commands/CreateAssign/CreateAssignCommandResponse.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation.Results;
+using ProjectManagement.Core.Base.Responses;
+using ProjectManagement.Core.UseCases.Assigns.Dto;
+
+namespace ProjectManagement.Core.UseCases.Assigns.Commands.CreateAssign
+{
+    public class CreateAssignCommandResponse : BaseResponse
+    {
+        public AssignDto AssignDto { get; set; }
+
+        public CreateAssignCommandResponse() : base() { }
+        public CreateAssignCommandResponse(ValidationResult validationResult) : base(validationResult) { }
+        public CreateAssignCommandResponse(string message) : base(message) { }
+        public CreateAssignCommandResponse(string message, bool success) : base(message, success) { }
+        public CreateAssignCommandResponse(AssignDto assignDto) => AssignDto = assignDto;
+
+    }
+}
